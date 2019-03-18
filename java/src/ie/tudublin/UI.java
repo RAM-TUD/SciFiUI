3ccda1941f5a4fa5f99e7ad5b8da2ca94dded913
RAM <C17456666@mydit.ie>
2019-03-18 15:23:10 +0000

    background images and target aquired

--- java/src/ie/tudublin/UI.java
+++ java/src/ie/tudublin/UI.java
@@ -1,72 +1,64 @@
 package ie.tudublin;
 
+
+
 import processing.core.PApplet;
+import processing.core.PImage;
+
+
 
 public class UI extends PApplet
 {
     
-
-    boolean[] keys = new boolean[1024];
-
-    public void keyPressed()
-    {
-        keys[keyCode] = true;
-    }
-    
-    public void keyReleased()
-    {
-        keys[keyCode] = true;
-    }
-
-    public boolean checkKey(int c)
-    {
-        return keys[c] || keys [Character.toUpperCase(c)];
-    }
-    
-
+    PImage alley;
+    PImage target;
     public void settings()
     {
-        // Use fullscreen instead of size to make your interface fullscreen
-        fullScreen(); 
-    }
-
-    public void setup()
-    {
-      
+        fullScreen();
     }
 
     public void draw()
     {
-        background(255,0,0);
-        design();
+        
+        background(alley);
+        //background(255,0,0);
         searchmode();
         loading();
+        design();
         
         
-
-        if (checkKey(LEFT))
-        {
-            System.out.println("Left arrow key pressed");
-        }
     }
+
+    public void setup()
+    {
+        System.out.println(width);
+        System.out.println(height);
+        alley = loadImage("street.jpg");
+        target = loadImage("target.jpg");
+    }
+
 
     public void searchmode()
     {
         
         float centerx = width / 2;
         float centery = height / 2;
+        
+        //fill(255);
+        stroke(255);
+        noFill();
+        ellipse(centerx, centery,width/(float)5.5,height/(float)5.5); //big circle
+        fill(255,0,0);
+        stroke(255);
+        line(centerx - width/11, centery, centerx + width/11, centery); //line across small circle
+        line(centerx,centery + 20, centerx, centery - 20); //tiny center line
         fill(255);
-        ellipse(centerx, centery,width/4,height/4); //big circle
-        ellipse(centerx, centery, width/6, height/6); //small circle
-        line(centerx - width/12, centery, centerx + width/12, centery); //line across small circle
-        line(centerx,centery + 10, centerx, centery - 10); //tiny center line
         textAlign(CENTER, CENTER);
         textSize(50);
         text("SEARCH MODE", centerx, centery + (height/6));
         textSize(30);
         
     }
-
     float loadingbar = 0;
     public void loading()
     {
@@ -74,6 +66,7 @@ public class UI extends PApplet
         textSize(30);
         float centerx = width/2;
         fill(255,0,0);
+        stroke(0);
         rect(centerx - 200, height - 150, 400,50);
         fill(255);
         rect(centerx - 200, height - 150, loadingbar, 50);
@@ -94,11 +87,11 @@ public class UI extends PApplet
         }
      
     }
-    
+    float r = 0;
     int y= 0;
     public void design()
     {
-       
+        
         //circle icon on top left
         int outline = 150;
         stroke(255);
@@ -113,11 +106,17 @@ public class UI extends PApplet
         fill(255);
         ellipse(100,100,30,30);
 
+        
+
+
+        //grid
         int gap = 40;
         int gridx = 950;
         int gridy = 350;
         
-        
+        image(target,gridx, gridy,280,200);
+        textSize(35);
+        text("AQUIRED TARGET",gridx + 135,gridy + 250);
         stroke(255);
         for(int i = 0; i < 6; i++)
         {
@@ -135,15 +134,20 @@ public class UI extends PApplet
             line(gridx,335,gridx,560);
             gridx += (gap - 5);
         }
+        
 
-        stroke(255);
-        line(0, y, width, y);
-        y+=5;
-        if (y > height)
+        //moving bar
+        int space = 30;
+        int xbar = 30;
+        line(30,height - 30, 340, height-30);
+        for(int i = 0; i < 12; i++)
         {
-            y = 0; 
+            int bar = (int)(Math.random() * 200 + 1);
+            rect(xbar,(height-30) - bar ,space,bar);
+            xbar += space;
         }
 
+        //appreaing buttons
         int timer = 0;
         int xbutton = width/2 - 200;
         int limit = width/2 + 200;
@@ -168,17 +172,20 @@ public class UI extends PApplet
             
         }
 
-         //moving bar
-         int space = 30;
-         int xbar = 30;
-         line(30,height - 30, 340, height-30);
-         for(int i = 0; i < 12; i++)
-         {
-             int bar = (int)(Math.random() * 200 + 1);
-             rect(xbar,(height-30) - bar ,space,bar);
-             xbar += space;
-         }
+        //line coming down
+        stroke(255);
+        line(0, y, width, y);
+        y+=5;
+        if (y > height)
+        {
+            y = 0; 
+        }
+        
+        
 
+       
     }
+
+   
 }
 
