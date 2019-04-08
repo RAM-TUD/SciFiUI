2b5bae82b74336543311f835725bfb038cb579d7
RAM <C17456666@mydit.ie>
2019-04-08 17:17:45 +0100

    designed weapon

--- java/src/ie/tudublin/Weapon.java
+++ java/src/ie/tudublin/Weapon.java
@@ -6,11 +6,13 @@ public class Weapon extends UIElements
 {
     private float size;
     private float length;
+    private float rotation;
     public Weapon(float x, float y, PApplet ui, float size, float length)
     {
         super(x,y,ui);
         this.size = size;
         this.length = length;
+        this.rotation = 0;
     }
 
     public void render()
@@ -22,7 +24,30 @@ public class Weapon extends UIElements
         ui.fill(255);
         ui.textSize(12);
         ui.text("WEAPON SYSTEM",x+size/2,y+size/3/2);
-        ui.text("ENABLE", x + size/2, y + length + 60 );
+        ui.text("ENABLE", x + size/2, y + length + 60);
+
+        ui.fill(255,50);
+        float trix = x + size/2;
+        float triy = y + size - 50;
+        ui.triangle(trix,triy,trix - 15, triy + 15, trix+15, triy + 15);
+        ui.rect(trix-15,triy+20,30,length/2);
+        ui.triangle(trix-30,triy-15 + length/2,trix-20,triy-15 + length/2, trix - 20, triy - 30 + length/2);
+        ui.triangle(trix+30,triy+15 + length/2,trix+20,triy+15 + length/2, trix + 20, triy + 30 + length/2);
+
+        int gap = 20;
+        for(int i = 0; i < 5; i++)
+        {
+            ui.line(x+5, y + size/2 + (i*gap),trix - 20, y + size/2 + (i*gap));
+            ui.line(x+ size - 5, y + size/2 + (i*gap),trix + 20, y + size/2 + (i*gap));
+        }
+        ui.pushMatrix();
+        ui.translate(trix - 45, triy + 15+length/2);
+        ui.rotate(rotation);
+        ui.noFill();
+        ui.stroke(255);
+        ui.arc(0,0,25,25,0,ui.PI);
+        ui.popMatrix();
+        rotation += 0.05f;
 
 
     }
